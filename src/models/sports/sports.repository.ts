ec616009fb98e4c1187bb5ebe3e8d85aa0390458
guidssantos <guilhermedssantos1@gmail.com>
2024-07-04 10:11:21 -0300

    feat: create repositories

--- src/models/sports/sports.repository.ts
+++ src/models/sports/sports.repository.ts
@@ -1,7 +1,43 @@
 import { PrismaClient } from '@prisma/client';
+import { SportsActivityDTO } from './dto';
 
 const prisma = new PrismaClient();
 
 export class SportsActivityRepository {
-    async findAll() {}
+    async findAll() {
+        return await prisma.sports.findMany();
+    }
+
+    async findById({ id }: string): Promise<any> {
+        return await prisma.sports.findUnique({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async create(data: SportsActivityDTO): Promise<any> {
+        // const { name, description, date } = data;
+
+        return await prisma.sports.create({
+            ...data,
+        });
+    }
+
+    async update(id: string, data: SportsActivityDTO): Promise<any> {
+        return await prisma.sports.update({
+            where: {
+                id: id,
+            },
+            data: data,
+        });
+    }
+
+    async delete(id: string): Promise<any> {
+        return await prisma.sports.delete({
+            where: {
+                id: id,
+            },
+        });
+    }
 }
