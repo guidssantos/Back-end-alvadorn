f2e820ef15a53c59af04704453f1fa38409c758a
guidssantos <guilhermedssantos1@gmail.com>
2024-07-04 10:11:42 -0300

    feat: create services

--- src/models/sports/sports.service.ts
+++ src/models/sports/sports.service.ts
@@ -1,4 +1,32 @@
+import { SportsActivityDTO } from './dto';
 import { SportsActivityRepository } from './sports.repository';
 export class SportsActivityService {
-    async findAll() {}
+    async findAll(sportsActivityRepository: SportsActivityRepository) {
+        const content = await sportsActivityRepository.findAll();
+        return content;
+    }
+
+    async findById(sportsActivityRepository: SportsActivityRepository, id: string) {
+        const content = await sportsActivityRepository.findById(id);
+        return content;
+    }
+
+    async create(sportsActivityRepository: SportsActivityRepository, data: SportsActivityDTO) {
+        const content = await sportsActivityRepository.create(data);
+        return content;
+    }
+
+    async update(
+        sportsActivityRepository: SportsActivityRepository,
+        id: string,
+        data: SportsActivityDTO
+    ) {
+        const content = await sportsActivityRepository.update(id, data);
+        return content;
+    }
+
+    async delete(sportsActivityRepository: SportsActivityRepository, id: string) {
+        const content = await sportsActivityRepository.delete(id);
+        return content;
+    }
 }
