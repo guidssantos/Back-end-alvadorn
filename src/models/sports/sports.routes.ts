fd618e39d563859666c6581244d9a76ad35d60d7
guidssantos <guilhermedssantos1@gmail.com>
2024-07-04 10:11:31 -0300

    feat: create routes

--- src/models/sports/sports.routes.ts
+++ src/models/sports/sports.routes.ts
@@ -6,5 +6,9 @@ const sportsActivityRouter = Router();
 const sportsActivityController = new SportsActivityController();
 
 sportsActivityRouter.get('/', routeAdapter(sportsActivityController.findAll));
+sportsActivityRouter.delete('/:id', routeAdapter(sportsActivityController.delete));
+sportsActivityRouter.get('/:id', routeAdapter(sportsActivityController.findById));
+sportsActivityRouter.post('/:id', routeAdapter(sportsActivityController.create));
+sportsActivityRouter.put('/:id', routeAdapter(sportsActivityController.update));
 
 export { sportsActivityRouter };
